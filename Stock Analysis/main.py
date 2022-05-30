12fd9051ef1ed3411566a106ce5a22bda60c545f
Muskan Jha <80477586+muskan300600@users.noreply.github.com>
2022-05-31 00:01:00 +0530

    Update main.py

--- Stock Analysis/main.py
+++ Stock Analysis/main.py
@@ -2,7 +2,7 @@
 """
 Created on Fri Sep 27 14:36:49 2019
 
-@author: Kaushik
+@author: Mini
 """
 #**************** IMPORT PACKAGES ********************
 from flask import Flask, render_template, request, flash, redirect, url_for
@@ -327,125 +327,7 @@ def insertintotable():
         print("Linear Regression RMSE:",error_lr)
         print("##############################################################################")
         return df, lr_pred, forecast_set, mean, error_lr
-    #**************** SENTIMENT ANALYSIS **************************
-    def retrieving_tweets_polarity(symbol):
-        stock_ticker_map = pd.read_csv('Yahoo-Finance-Ticker-Symbols.csv')
-        stock_full_form = stock_ticker_map[stock_ticker_map['Ticker']==symbol]
-        symbol = stock_full_form['Name'].to_list()[0][0:12]
-
-        auth = tweepy.OAuthHandler(ct.consumer_key, ct.consumer_secret)
-        auth.set_access_token(ct.access_token, ct.access_token_secret)
-        user = tweepy.API(auth)
-        
-        tweets = tweepy.Cursor(user.search, q=symbol, tweet_mode='extended', lang='en',exclude_replies=True).items(ct.num_of_tweets)
-        
-        tweet_list = [] #List of tweets alongside polarity
-        global_polarity = 0 #Polarity of all tweets === Sum of polarities of individual tweets
-        tw_list=[] #List of tweets only => to be displayed on web page
-        #Count Positive, Negative to plot pie chart
-        pos=0 #Num of pos tweets
-        neg=1 #Num of negative tweets
-        for tweet in tweets:
-            count=20 #Num of tweets to be displayed on web page
-            #Convert to Textblob format for assigning polarity
-            tw2 = tweet.full_text
-            tw = tweet.full_text
-            #Clean
-            tw=p.clean(tw)
-            #print("-------------------------------CLEANED TWEET-----------------------------")
-            #print(tw)
-            #Replace &amp; by &
-            tw=re.sub('&amp;','&',tw)
-            #Remove :
-            tw=re.sub(':','',tw)
-            #print("-------------------------------TWEET AFTER REGEX MATCHING-----------------------------")
-            #print(tw)
-            #Remove Emojis and Hindi Characters
-            tw=tw.encode('ascii', 'ignore').decode('ascii')
-
-            #print("-------------------------------TWEET AFTER REMOVING NON ASCII CHARS-----------------------------")
-            #print(tw)
-            blob = TextBlob(tw)
-            polarity = 0 #Polarity of single individual tweet
-            for sentence in blob.sentences:
-                   
-                polarity += sentence.sentiment.polarity
-                if polarity>0:
-                    pos=pos+1
-                if polarity<0:
-                    neg=neg+1
-                
-                global_polarity += sentence.sentiment.polarity
-            if count > 0:
-                tw_list.append(tw2)
-                
-            tweet_list.append(Tweet(tw, polarity))
-            count=count-1
-        if len(tweet_list) != 0:
-            global_polarity = global_polarity / len(tweet_list)
-        else:
-            global_polarity = global_polarity
-        neutral=ct.num_of_tweets-pos-neg
-        if neutral<0:
-        	neg=neg+neutral
-        	neutral=20
-        print()
-        print("##############################################################################")
-        print("Positive Tweets :",pos,"Negative Tweets :",neg,"Neutral Tweets :",neutral)
-        print("##############################################################################")
-        labels=['Positive','Negative','Neutral']
-        sizes = [pos,neg,neutral]
-        explode = (0, 0, 0)
-        fig = plt.figure(figsize=(7.2,4.8),dpi=65)
-        fig1, ax1 = plt.subplots(figsize=(7.2,4.8),dpi=65)
-        ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', startangle=90)
-        # Equal aspect ratio ensures that pie is drawn as a circle
-        ax1.axis('equal')  
-        plt.tight_layout()
-        plt.savefig('static/SA.png')
-        plt.close(fig)
-        #plt.show()
-        if global_polarity>0:
-            print()
-            print("##############################################################################")
-            print("Tweets Polarity: Overall Positive")
-            print("##############################################################################")
-            tw_pol="Overall Positive"
-        else:
-            print()
-            print("##############################################################################")
-            print("Tweets Polarity: Overall Negative")
-            print("##############################################################################")
-            tw_pol="Overall Negative"
-        return global_polarity,tw_list,tw_pol,pos,neg,neutral
-
-
-    def recommending(df, global_polarity,today_stock,mean):
-        if today_stock.iloc[-1]['Close'] < mean:
-            if global_polarity > 0:
-                idea="RISE"
-                decision="BUY"
-                print()
-                print("##############################################################################")
-                print("According to the ML Predictions and Sentiment Analysis of Tweets, a",idea,"in",quote,"stock is expected => ",decision)
-            elif global_polarity <= 0:
-                idea="FALL"
-                decision="SELL"
-                print()
-                print("##############################################################################")
-                print("According to the ML Predictions and Sentiment Analysis of Tweets, a",idea,"in",quote,"stock is expected => ",decision)
-        else:
-            idea="FALL"
-            decision="SELL"
-            print()
-            print("##############################################################################")
-            print("According to the ML Predictions and Sentiment Analysis of Tweets, a",idea,"in",quote,"stock is expected => ",decision)
-        return idea, decision
-
-
-
-
-
+   
     #**************GET DATA ***************************************
     quote=nm
     #Try-except to check if valid stock symbol
